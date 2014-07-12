7971ad8127b2816ca4d55495392813f729f32e6a
Manuel Farina <manuel.farina@zhdk.ch>
2014-07-12 21:17:36 +0200

    Created admininterface

---
+++ admin.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<form action="" method="post">
+			Titel:<br> 
+			<input type="text" name="titel"><br>
+			Inhalt:<br>
+			<textarea id="text" name="inhalt" cols="50" rows="25" resize: "none" placeholder="Inhalt hier reinschreiben"></textarea> 
+			<input type="submit" value="submit" name="submit">
+		</form>
+	</body>
+</html>
+
+<?php
+
+include 'save.php';
